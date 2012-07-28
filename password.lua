eca56e0bb59ccdb23aa789c32b5bd077dfe1040f
Evan Sims <evansims@gmail.com>
2012-07-28 00:47:22 -0500

    Password protect a door using a terminal password.

---
+++ password.lua
@@ -0,0 +1,44 @@
+--[[
+
+Lock a door using a terminal password.
+Blocks CTRL+T program termination.
+
+To start on boot, edit your startup file to:
+shell.run("password")
+
+--]]
+
+function clear()
+	term.clear()
+	term.setCursorPos(1,1)
+end
+
+function correct()
+	clear()
+
+	print("Password accepted.")
+	redstone.setOutput("left", true)
+	sleep(10)
+
+	clear()
+	redstone.setOutput("left", false)
+	shell.run("password")
+end
+
+function wrong()
+	clear()
+	print("Password rejected. Try again.")
+end
+
+function main()
+	repeat
+	os.pullEvent = os.pullEventRaw
+	local input = io.read()
+	if input ~= "password" then wrong() end
+	until input == "password"
+
+	correct()
+end
+
+print("Password required:")
+main()
